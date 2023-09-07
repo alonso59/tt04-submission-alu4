d881c6ec4d7732826b826046397f44e2c40208d8
alonso <germanpinedo13@gmail.com>
2023-09-07 14:57:10 -0600

    Update tt_um_alu4_alonso59.v

--- src/tt_um_alu4_alonso59.v
+++ src/tt_um_alu4_alonso59.v
@@ -22,15 +22,38 @@ pwm DUT(
 );
 
 	ALU_4bit alu1(
-	.Out(alu_out[3:0]), 
+	.A({ui_in[7:5],1'b0}), 
+	.B({ui_in[7:5],1'b0}), 
+	.Opcode(ui_in[3:0]),
+	
 	.Z(alu_out[4]), 
 	.C(alu_out[5]), 
 	.V(alu_out[6]), 
 	.P(alu_out[7]), 
-	.A({ui_in[7:5],1'b0}), 
-	.B({ui_in[7:5],1'b0}), 
-	.Opcode(ui_in[3:0])
+	
+	.Out(alu_out[3:0]), 
 	);
+endmodule
+module ALU_4bit(Out, Z, C, V, P, A, B, Opcode);
+
+	input [3:0] A, B, Opcode;
+	output Z, C, V, P;
+	output [3:0] Out;
+	wire temp_C, temp_V;
+	wire [3:0] shift_out, arith_out, comp_out, logical_out;
+
+	Shifter s(shift_out, A, B, Opcode);
+	Arithmetic a(arith_out, temp_C, temp_V, A, B, Opcode);
+	Comparator c(comp_out, A, B, Opcode);
+	Logical l(logical_out, A, B, Opcode);
+	
+	MUX m(Out, shift_out, arith_out, logical_out, comp_out, Opcode);
+	
+	assign C = (Opcode > 3 && Opcode < 8) ? temp_C: 1'b0;
+	assign V = (Opcode > 3 && Opcode < 8) ? temp_V: 1'b0;
+	assign P = Out[3] ^ Out[2] ^ Out[1] ^ Out[0];
+	assign Z = (Out[3] == 0) & (Out[2] == 0) & (Out[1] == 0) & (Out[0] == 0);
+	
 endmodule
 
 module Shifter(shift_out, A, B, Opcode);
@@ -63,27 +86,7 @@ module MUX(Out, shift_out, arith_out, logical_out, compare_out, Opcode);
 
 endmodule
 
-module ALU_4bit(Out, Z, C, V, P, A, B, Opcode);
 
-	input [3:0] A, B, Opcode;
-	output Z, C, V, P;
-	output [3:0] Out;
-	wire temp_C, temp_V;
-	wire [3:0] shift_out, arith_out, comp_out, logical_out;
-
-	Shifter s(shift_out, A, B, Opcode);
-	Arithmetic a(arith_out, temp_C, temp_V, A, B, Opcode);
-	Comparator c(comp_out, A, B, Opcode);
-	Logical l(logical_out, A, B, Opcode);
-	
-	MUX m(Out, shift_out, arith_out, logical_out, comp_out, Opcode);
-	
-	assign C = (Opcode > 3 && Opcode < 8) ? temp_C
-				: 1'b0;
-	
-	assign Z = (Out[3] == 0) & (Out[2] == 0) & (Out[1] == 0) & (Out[0] == 0);
-	
-endmodule
 
 module Comparator(comp_out, A, B, Opcode);
 
